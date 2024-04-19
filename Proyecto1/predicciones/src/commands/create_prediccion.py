9ceb54dc8d3c2383404102e6ff8104a4c40a5904
j.jauregui@uniandes.edu.co <j.jauregui@uniandes.edu.co>
2024-04-19 11:06:28 -0500

    Intento con FastAPI

--- Proyecto1/predicciones/src/commands/create_prediccion.py
+++ Proyecto1/predicciones/src/commands/create_prediccion.py
@@ -2,23 +2,37 @@ from .base_command import BaseCommannd
 from ..models.prediccion import Prediccion, PrediccionSchema, CreatedPrediccionJsonSchema
 from ..session import Session
 from ..errors.errors import IncompleteParams
+from joblib import load
+import pandas as pd
+import os
 
 
 class CreatePrediccion(BaseCommannd):
     def __init__(self, data):
         self.data = data
 
+    def load_model(self):
+        path = os.path.join(os.path.dirname(__file__), 'assets/modelLemmatizer.joblib')
+        model = load(path)
+        try:
+            return model
+        except FileNotFoundError:
+            raise FileNotFoundError("No se encontró el modelo en la ruta especificada")
+
     def execute(self):
         try:
-            posted_prediccion = PrediccionSchema(
-                only=('resena', 'clasificacion', 'probabilidad')).load(self.data)
-            prediccion = Prediccion(**posted_prediccion)
-            session = Session()
+            posted_prediccion_data = PrediccionSchema(
+                only=('resena',)).load(self.data)
 
-            session.add(prediccion)
+            df = pd.DataFrame(posted_prediccion_data, columns=posted_prediccion_data.keys(), index=[0])
+            model = self.load_model()
+            result = model.predict(df)
+            
+            session = Session()
+            session.add(result)
             session.commit()
 
-            new_prediccion = CreatedPrediccionJsonSchema().dump(prediccion)
+            new_prediccion = CreatedPrediccionJsonSchema().dump(result)
             session.close()
 
             return new_prediccion
