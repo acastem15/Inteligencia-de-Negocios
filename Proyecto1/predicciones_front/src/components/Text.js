1e4b5fe735dc8a5a4012e46d82474e3b164adc68
luna-as18 <85699323+luna-as18@users.noreply.github.com>
2024-04-20 14:14:59 -0500

    front

--- Proyecto1/predicciones_front/src/components/Text.js
+++ Proyecto1/predicciones_front/src/components/Text.js
@@ -20,7 +20,7 @@ function Text() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ resena: inputValue }),
+        body: JSON.stringify({ text: inputValue }),
       });
 
       if (!response.ok) {
@@ -28,7 +28,8 @@ function Text() {
       }
 
       const data = await response.json();
-      setPredictionResult(`Predicción: ${data.prediccion}, Probabilidad: ${data.probabilidad}`);
+      // Assuming your backend returns an object with keys "prediction" and "probability"
+      setPredictionResult(`Prediction: ${data.clasificacion}, Probability: ${data.probabilidad}`);
     } catch (error) {
       console.error('Error predicting:', error);
     }
@@ -36,7 +37,7 @@ function Text() {
 
   return (
     <div>
-        {/* Header */}
+      {/* Header */}
       <header className="header">
         <img src="/flight.png" alt="Flight" />
         <span>Tourify</span>
@@ -48,22 +49,53 @@ function Text() {
         <img src="/trip2.jpg" alt="Trip 2" className="image" />
         <img src="/trip3.jpg" alt="Trip 3" className="image" />
       </div>
-       
-      <div className="textbox-container">
-        {/* Textbox */}
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleChange}
-          placeholder="Ingrese la reseña aquí..."
-          className="textbox"
-        />
-        
-        {/* Button */}
-        <button onClick={handlePrediction} className="button">Predecir</button>
+
+      {/* Texto */}
+      <h1 className="title">Bienvenido a Tourify</h1>
+      <p className="subtitle">¡Descubre los secretos para tener a un turista feliz!</p>
+
+      {/* Descripción */}
+      <div className="description-container">
+        <div className="description">
+          <p><strong>¿Te interesa impulsar el turismo en Colombia?</strong></p>
+          <p>Nuestra aplicación está diseñada para ayudarte a explorar y promover los destinos turísticos más increíbles de nuestro país.</p>
+          
+          <p><strong>¿Quiénes pueden beneficiarse de esta aplicación?</strong></p>
+          <p>Principalmente, el personal del Ministerio de Comercio, Industria y Turismo de Colombia, así como miembros de la Asociación Hotelera y Turística de Colombia (COTELCO), y gerentes y propietarios de cadenas hoteleras como Hilton, Hoteles Estelar, Holiday Inn, y hoteles más pequeños ubicados en varios municipios de Colombia.</p>
+          
+          <p><strong>¿Qué puedes hacer con nuestra aplicación?</strong></p>
+          <p>Podrás analizar las características de los sitios turísticos que influyen en su atractivo para los turistas. ¿Quieres comparar estos sitios con otros que han recibido bajas recomendaciones? ¡Puedes hacerlo aquí también! Además, nuestra aplicación te ayudará a determinar la calificación potencial de un sitio por parte de los turistas.</p>
+          
+          <p><strong>¿Por qué es importante esta aplicación para ti?</strong></p>
+          <p>Porque te proporcionará información valiosa y análisis predictivos que pueden utilizarse para identificar oportunidades de mejora. ¿Quieres aumentar la popularidad de los sitios turísticos y fomentar el turismo en Colombia? ¡Entonces esta aplicación es tu aliado perfecto!</p>
+          
+          <p><strong>¡Únete a nosotros y comienza a descubrir las maravillas de Colombia hoy mismo!</strong></p>
+        </div>
       </div>
 
-      {/* Display prediction result */}
+      {/* Cuadro de texto y botón de predicción */}
+      <div className="textbox-container">
+        <div className="left-image-container">
+          <img src="/trip4.jpg" alt="Trip 4" className="left-image" />
+        </div>
+        <div className="review-container">
+          <h2>Escribe tu reseña y nuestro modelo predecirá qué hace a tu cliente feliz:</h2>
+          <div className="review-box">
+            {/* Cuadro de texto */}
+            <textarea
+              value={inputValue}
+              onChange={handleChange}
+              placeholder="Ingrese la reseña aquí..."
+              className="textbox"
+            />
+            
+            {/* Botón de predicción */}
+            <button onClick={handlePrediction} className="button">Predecir</button>
+          </div>
+        </div>
+      </div>
+
+      {/* Resultado de la predicción */}
       <div>{predictionResult}</div>
     </div>
   );
